f4ac28e51dbff64107943c8f9518ff35dc183e0a
rohitsar <rohitsar@umich.edu>
2023-12-01 18:29:40 -0500

    upload pictures works

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import numpy as np
 import os
 import subprocess
 app = Flask(__name__)
-
+CORS(app)
+UPLOAD_FOLDER = '/Users/rohit/Desktop/Umich2ndyear/Fall2023/EECS 442/442_Project/background/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/background')
 def hello():
@@ -13,6 +16,28 @@ def hello():
     output = subprocess.call("CUDA_VISIBLE_DEVICES=0 python3 test_background-matting_image.py -m real-fixed-cam -i colab_inputs/input/ -o colab_inputs/output/ -tb colab_inputs/background/0001.png", shell=True)
     print(output)
     return 'Hello, World!'
+
+@app.route('/upload-multiple', methods=['POST'])
+def upload_multiple_files():
+    try:
+        # Check if the post request has the file part
+        if 'files[]' not in request.files:
+            return jsonify({'error': 'No files part in the request'}), 400
+
+        files = request.files.getlist('files[]')
+
+        uploaded_files = []
+
+        for file in files:
+            if file:
+                # Save the file to the UPLOAD_FOLDER
+                filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+                file.save(filename)
+                uploaded_files.append(filename)
+
+        return jsonify({'message': 'Files uploaded successfully', 'files': uploaded_files})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
--- iswbbb-frontend/src/app/background/page.tsx
+++ iswbbb-frontend/src/app/background/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/header";
 import List from "@/components/list"
+import { useEffect } from "react";
 
 export default function Home() {
+
   return (
     <div>
-        Hello world
+      hello
     </div>
   )
 }
