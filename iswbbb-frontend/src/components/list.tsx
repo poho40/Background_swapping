f365a8c06d5b68b39ccfc8e7089d4565bab40ff4
rohitsar <rohitsar@umich.edu>
2023-12-01 18:46:21 -0500

    redirect to background after submitting form

--- iswbbb-frontend/src/components/list.tsx
+++ iswbbb-frontend/src/components/list.tsx
@@ -1,13 +1,12 @@
 "use client";
 import React, { useState } from 'react'
+import { useRouter } from 'next/navigation';
 
 
 import {Resizable} from 're-resizable';
 import Draggable from 'react-draggable';
 
 import Card from './card';
-import multer from 'multer';
-import path from 'path';
 
 const imageUrl =
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe7Ki-ys2G_MMb_xCrY7nAf87F5ZiIOyCh4f5H_JCTTtMSMLCL';
@@ -19,7 +18,7 @@ const list:React.FC<listProps> = () => {
     const [cards, setCards] = useState([{url: "https://cdn.mos.cms.futurecdn.net/dPo92zYeAz7Joxh7HWooJ3-1200-80.jpg.webp"}]);
     
     const [files, setFiles] = useState(null);
-
+    const router = useRouter();
     const handleFileChange = (e) => {
         setFiles(e.target.files);
     };
@@ -49,6 +48,7 @@ const list:React.FC<listProps> = () => {
             // Handle the response as needed
             const data = await response.json();
             console.log(data);
+            router.push('/background');
         } catch (error) {
             console.error('Error uploading files:', error);
         }
@@ -57,7 +57,7 @@ const list:React.FC<listProps> = () => {
     return (
         <div>
             <h1>Upload Multiple Files</h1>
-            <form onSubmit={handleSubmit} encType="multipart/form-data">
+            <form action='/background' onSubmit={handleSubmit} encType="multipart/form-data">
                 <input type="file" name="files" multiple onChange={handleFileChange} />
                 <button type="submit">Upload Files</button>
             </form>
